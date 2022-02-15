b011dcde6b255aa9f2711f6d6da6d2ad957a6ab2
Andreas Alfons <alfons@ese.eur.nl>
2022-02-15 20:43:20 +0100

    proof-of-concept version of histogramSPSS()

--- R/histSPSS.R
+++ R/histSPSS.R
@@ -8,12 +8,13 @@
 #' @export
 
 histogramSPSS <- function(data, variable, normal = FALSE,
-                          bins = NULL, expand = 0.05, ...) {
+                          bins = NULL, digits = 3, expand = 0.05, ...) {
   # initializations
   data <- as.data.frame(data)
   variable <- as.character(variable)
   if (length(variable) == 0) stop("a variable to display must be specified")
   variable <- variable[1]
+  normal <- isTRUE(normal)
   # extract variable of interest
   x <- data[, variable]
   # if necessary, determine the number of bins
@@ -28,6 +29,10 @@ histogramSPSS <- function(data, variable, normal = FALSE,
   m <- mean(x)
   s <- sd(x)
   n <- length(x)
+  # construct text for summary statistics to be included in top right corner
+  format <- paste0("%.", digits, "f")
+  statistics <- paste0("Mean = ", format, "\nStd. Dev. = ", format, "\nN = %d")
+  statistics <- sprintf(statistics, m, s, n)
   # create an initial histogram
   initial <- ggplot(data, aes_string(x = variable)) +
     geom_histogram(bins = bins)
@@ -43,6 +48,8 @@ histogramSPSS <- function(data, variable, normal = FALSE,
   # adjusted <- 0.95 * binwidth / 2
   # histogram$xmin <- histogram$x - adjusted
   # histogram$xmax <- histogram$x + adjusted
+  # determine range of y-axis
+  ylim <- c(0, max(histogram$count))
   # initialize plot as rectangles for histogram
   p <- ggplot() +
     geom_rect(aes(xmin = xmin, xmax = xmax, ymin = 0, ymax = count),
@@ -50,29 +57,38 @@ histogramSPSS <- function(data, variable, normal = FALSE,
   # if requested, compute normal density and it add to plot
   if (normal) {
     # define grid for normal density
-    grid <- seq(from = xlim[1], to = xlim[2], length.out = 100)
+    grid <- seq(from = xlim[1], to = xlim[2], length.out = 1000)
     # compute rescaled normal density
     whichMax <- which.max(histogram$count)
-    density <- dnorm(grid, mean = m, sd = s) *
-      histogram[whichMax, "count"] / histogram[whichMax, "density"]
+    y <- dnorm(grid, mean = m, sd = s) * histogram[whichMax, "count"] /
+      histogram[whichMax, "density"]
+    # update upper limit of y-axis
+    ylim[2] <- max(ylim[2], y)
     # combine information into data.frame
-    density <- data.frame(x = grid, y = density)
+    density <- data.frame(x = grid, y = y)
     # add density to plot
     p <- p +
       geom_line(aes(x = x, y = y), data = density, color = "black", size = 1)
   }
+  # expand upper limit of y-axis according to expansion factor
+  ylim[2] <- ylim[2] + expand * diff(ylim)
   # finalize appearance of plot
   p <- p +
     geom_hline(yintercept = 0) +
     geom_vline(xintercept = xlim[1]) +
     theme_bw() +
-    theme(axis.title = element_text(face = "bold", size = 12),
+    theme(axis.title = element_text(face = "bold"),
+          axis.text.y.right = element_text(vjust = 1),
           axis.ticks.length = unit(0, "points"),
           panel.border = element_blank(),
           panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(color = gray(0.66)),
           panel.grid.minor = element_blank()) +
-    scale_x_continuous(limits = xlim, expand = expansion(mult = c(0, 0))) +
-    scale_y_continuous(expand = expansion(mult = c(0, expand))) +
+    scale_x_continuous(limits = xlim, expand = expansion(mult = 0)) +
+    scale_y_continuous(limits = ylim, expand = expansion(mult = 0),
+                       sec.axis = dup_axis(breaks = ylim[2],
+                                           name = NULL,
+                                           labels = statistics)) +
     labs(x = variable, y = "Frequency")
   # return plot
   p
